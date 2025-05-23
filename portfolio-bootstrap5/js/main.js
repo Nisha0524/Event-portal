6d9422644450fb11d1e3a34085be99166eca7c35
Nisha0524 <nishadj0524@gmail.com>
2025-05-23 12:08:47 +0530

    Add files via upload

---
+++ portfolio-bootstrap5/js/main.js
@@ -0,0 +1,9 @@
+
+const contactForm = document.querySelector("form");
+
+contactForm.addEventListener("submit", function (event) {
+  event.preventDefault(); 
+
+
+  alert("Thanks for reaching out! I'll try to respond soon.");
+});
